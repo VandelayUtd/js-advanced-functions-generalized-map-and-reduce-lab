b9f3bf5435164a207d577c032a2aba132677c840
VandelayUtd <drewhadley.guitar@gmail.com>
2021-10-29 16:06:06 -0400

    commit tree 60a1525a6428b7ca12c3792110cedbeb98644b3e

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your functions here
+function map(src, callback){
+    let newArray = []
+    for(let i = 0; i < src.length; i++){
+        newArray.push(callback(src[i]))
+    }
+
+    return newArray
+}
+
+function reduce(src, callback, start){
+    let total; 
+    if (start){
+        total = start
+        for (let i = 0; i < src.length; i++){
+            total = callback(src[i], total)
+        }
+        return total
+    }else {
+        total = src[0]
+        for (let i = 1; i < src.length; i++){
+            total = callback(src[i], total)
+        }
+        return total 
+
+    }
+}
